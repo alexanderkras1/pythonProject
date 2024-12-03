31c038c914157d764fa8981923ffd85113c084d9
alexanderkras1 <skrasnochchek@gmail.com>
2024-12-03 23:01:52 +0200

    Solved homework 2.2

--- lesson_3.py
+++ lesson_3.py
@@ -1,1 +1,19 @@
-aaaa
+number = int(input("Введите 5-ти значное число: "))
+
+reversed_number = 0
+
+reversed_number += (number % 10) * 10000
+number = number // 10
+
+reversed_number += (number % 10) * 1000
+number = number // 10
+
+reversed_number += (number % 10) * 100
+number = number // 10
+
+reversed_number += (number % 10) * 10
+number = number // 10
+
+reversed_number += number % 10
+
+print("Перевернутое число:", reversed_number)
