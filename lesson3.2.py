280f9a5918856ce5245ed2878342891e6c976d11
alexanderkras1 <skrasnochchek@gmail.com>
2024-12-10 18:21:06 +0200

    solved homework 3.2

---
+++ lesson3.2.py
@@ -0,0 +1,23 @@
+my_list = []
+
+len(my_list) <= 1
+result = my_list = []
+
+print(result)
+# print(list1)
+
+# [1, 2, 3, 4, 5, 6] => [6, 1, 2, 3, 4, 5]
+# my_list = [1, 2, 3, 4, 5, 6]
+# list1 = [my_list[-1]] + my_list[:5]
+
+# [1] => [1]
+# my_list = [1]
+# len(my_list) <= 1
+# result = my_list = [1]
+
+# [0] => [0]
+# my_list = []
+# len(my_list) <= 1
+# result = my_list = []
+
+
