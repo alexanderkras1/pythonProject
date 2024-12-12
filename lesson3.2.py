ad31a846734e8baa9bdd69606291082f0746f88b
alexanderkras1 <skrasnochchek@gmail.com>
2024-12-12 19:10:33 +0200

    Update task 3.2

--- lesson3.2.py
+++ lesson3.2.py
@@ -1,14 +1,53 @@
-my_list = []
-
-len(my_list) <= 1
-result = my_list = []
+my_list = [1, 2, 3, 4, 5, 6]
+if len(my_list) > 1:
+    result = [my_list[-1]] + my_list[:-1]
+else:
+    result = my_list
 
 print(result)
-# print(list1)
 
-# [1, 2, 3, 4, 5, 6] => [6, 1, 2, 3, 4, 5]
-# my_list = [1, 2, 3, 4, 5, 6]
-# list1 = [my_list[-1]] + my_list[:5]
+my_list = [1]
+if len(my_list) > 1:
+     result = [my_list[-1]] + my_list[:-1]
+else:
+    result = my_list
+
+print(result)
+
+my_list = [0]
+if len(my_list) > 1:
+    result = [my_list[-1]] + my_list[:-1]
+else:
+    result = my_list
+
+print(result)
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+if len(my_list) > 1:
+    result = [my_list[-1]] + my_list[:-1]
+else:
+    result = my_list
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # [1] => [1]
 # my_list = [1]
