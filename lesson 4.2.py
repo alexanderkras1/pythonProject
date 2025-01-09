ead695b466eafdd14c1717b099e2de836e7825df
alexanderkras1 <skrasnochchek@gmail.com>
2025-01-09 18:53:52 +0200

    dded lesson 4.2 homework

--- lesson 4.2.py
+++ lesson 4.2.py
@@ -1,4 +1,27 @@
+my_list = [0, 1, 7, 2, 4, 8]
+first_list = my_list[::2]
+sum_l = first_list[0] + first_list[1] + first_list[2]
+res = sum_l * my_list[-1]
 
-# [0, 1, 7, 2, 4, 8] => (0 + 7 + 4) * 8 = 88
+print(res)
+
 
 my_list = [1, 3, 5]
+first_list = my_list[::2]
+sum_l = first_list[0] + first_list[1]
+res = sum_l * my_list[-1]
+
+print(res)
+
+my_list = [6]
+first_list = my_list[::2]
+sum_l = first_list[0]
+res = sum_l * my_list[-1]
+print(res)
+
+
+my_list =[]
+first_list = my_list[::2]
+sum_l = first_list[0] + first_list[1] if len(first_list) > 1 else sum(first_list)
+res = sum_l * my_list[-1] if my_list else 0
+print(res)
