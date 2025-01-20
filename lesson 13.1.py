c8343268251057a6aace3f2f2dbb4e807ab50c3b
alexanderkras1 <skrasnochchek@gmail.com>
2025-01-21 01:39:34 +0200

    solved homework 13.1.py

---
+++ lesson 13.1.py
@@ -0,0 +1,58 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.age} years old, {self.gender}"
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, Record Book: {self.record_book}"
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        self.group = {s for s in self.group if s.last_name != last_name}
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = "\n".join(str(student) for student in self.group)
+        return f"Group Number: {self.number}\nStudents:\n{all_students}"
+
+
+# Testing
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AM142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AM145')
+gr = Group('P01')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен вернуть студента'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
